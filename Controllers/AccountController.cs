f854eb12679a786725e94952d4b9702a13981b71
Jakub <jakub.jakacki18@gmail.com>
2024-03-14 01:02:53 +0100

    -Poprawa routingu (od teraz adresy stron to stona domowa/signin lub signup z pominięciem nazwy kontrolera

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,12 +6,12 @@ namespace Forum.Controllers
 {
     public class AccountController : Controller
     {
-
-
+        [Route("signin", Name = "SignIn")]
         public IActionResult SignIn()
         {
             return View();
         }
+        [Route("signup", Name = "SignUp")]
         public IActionResult SignUp()
         {
             return View();
