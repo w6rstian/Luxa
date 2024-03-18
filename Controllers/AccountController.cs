4a2493cd39ea884d9f163da9751f269e8ec05f4d
Jakub Jakacki <jakub.jakacki18@gmail.com>
2024-03-18 19:37:19 +0100

    -Rozwój tworzenia użytkownika z poziomu panelu konfiguracyjnego -Dodanie pola kategoria w klasie modelowej UserModel -Dodanie wyświetlania opisów do elementów klasy modelowej UserModel

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,37 +1,59 @@
 ﻿using Luxa.Data;
 using Luxa.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Luxa.Controllers
 {
-    public class AccountController : Controller
-    {
-        private ApplicationDbContext context;
-        public AccountController(ApplicationDbContext context)
-        {
-            this.context = context;
-        }
+	public class AccountController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+		public AccountController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        [Route("signin", Name = "SignIn")]
-        public IActionResult SignIn()
-        {
-            return View();
-        }
-        [Route("signup", Name = "SignUp")]
-        public IActionResult SignUp()
-        {
-            return View();
-        }
-        public IActionResult UsersList()
-        {
-            var users = context.Users.ToList();
-            
+		[Route("signin", Name = "SignIn")]
+		public IActionResult SignIn()
+		{
+			return View();
+		}
+		[Route("signup", Name = "SignUp")]
+		public IActionResult SignUp()
+		{
+			return View();
+		}
+		public IActionResult UsersList()
+		{
+			var users = _context.Users.ToList();
+			return View(users);
+		}
 
+		public IActionResult CreateUser()
+		{
+			return View();
+		}
 
+		[HttpPost]
+		public IActionResult CreateUser(UserModel userData)
+		{
+			if (ModelState.IsValid)
+			{
+				/*var user = new UserModel()
+				{
+					FirstName = ,
+					LastName = ,
+					Nickname = ,
+					Password = ,
+					Email = ,
+					Country = ,
+					PhoneNumber = ,
+					Category = ,
 
-            return View(users);
-        }
+				}*/
+			}
 
-    }
+			return View();
+		}
+
+	}
 }
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,26 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Luxa.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Luxa.Models
 {
-    public class UserModel
-    {
-        //klucz główny
-        [Key]
-        public int Id { get; set; }
-        //atrybut do walidacji adresu e-mail
-        [DataType(DataType.EmailAddress)]
-        public string? Email { get; set; }
-        public string? Country { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        //atrybut do numeru telefonu
-        [DataType(DataType.PhoneNumber)]
-        public int? PhoneNumber { get; set; }
-        [Display(Name="Nazwa użytkownika")]
-        public string Nickname { get; set;}
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Hasło")]
-        public string Password { get; set;}
-    }
+	public class UserModel
+	{
+		//klucz główny
+		[Key]
+		//automatycznie będzie dopisywać id
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		[Display(Name = "Kategoria użytkownika")]
+		public CategoryOfUser Category { get; set; }
+		//atrybut do walidacji adresu e-mail
+		[DataType(DataType.EmailAddress)]
+		[Display(Name = "Adres E-mail")]
+		public string? Email { get; set; }
+		[Display(Name = "Kraj")]
+		public string? Country { get; set; }
+		[Display(Name = "Imie")]
+		public string FirstName { get; set; }
+		[Display(Name = "Nazwisko")]
+		public string LastName { get; set; }
+		//atrybut do numeru telefonu
+		[DataType(DataType.PhoneNumber)]
+		[Display(Name = "Numer telefonu")]
+		public int? PhoneNumber { get; set; }
+		[Display(Name = "Nazwa użytkownika")]
+		public string Nickname { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Hasło")]
+		public string Password { get; set; }
+	}
 }
