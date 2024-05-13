e03d8cd885ffa82dd5b456e91e2a97d55360ad0e
Jakub <jakub.jakacki18@gmail.com>
2024-05-13 14:45:31 +0200

    -Utworzony formularz kontaku

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Luxa.Data;
+using Luxa.Data.Enums;
+using System.Reflection;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace Luxa.Controllers
 {
@@ -8,5 +13,25 @@ namespace Luxa.Controllers
         {
             return View();
         }
-    }
+		[HttpGet]
+		public IActionResult GetDetailedCategory(string selectedValue)
+        {
+			CategoryOfContact category = (CategoryOfContact)Enum.Parse(typeof(CategoryOfContact), selectedValue);
+			List<string> filteredText = typeof(DatailedContactCategories)
+										   .GetFields(BindingFlags.Public | BindingFlags.Static)
+										   .Where(field => ((ValueTuple<CategoryOfContact, string>)field.GetValue(null)).Item1 == category)
+										   .Select(field => ((ValueTuple<CategoryOfContact, string>)field.GetValue(null)).Item2)
+										   .ToList();
+			List<string> filteredValue = typeof(DatailedContactCategories)
+										   .GetFields(BindingFlags.Public | BindingFlags.Static)
+										   .Where(field => ((ValueTuple<CategoryOfContact, string>)field.GetValue(null)).Item1 == category)
+										   .Select(field => field.Name)
+										   .ToList();
+
+			var detailedCategories = new List<SelectListItem>();
+			//detailedCategories.AddRange(filteredList.Select(p => new SelectListItem { Text = p.Item2, Value = p.Item1 }));
+			return Json(new { text = filteredText, value = filteredValue });
+		}
+
+	}
 }
---
+++ Models/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using Luxa.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Luxa.Models
+{
+	public class ContactModel
+	{
+		public int Id { get; set; }
+		public UserModel Sender { get; set; } = default!;
+		[EnumDataType(typeof(CategoryOfContact))]
+		public CategoryOfContact Category { get; set; } = default!;
+		public string DetailedCategory { get; set; } = default!;
+		public string Description { get; set; } = default!;
+
+	}
+}
